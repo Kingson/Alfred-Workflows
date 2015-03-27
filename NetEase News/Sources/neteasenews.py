a653d61ad6b611d2ee8b02f0dc1d6a3a7035795a
Kingson Zhou <xiaomayi0323@gmail.com>
2015-03-27 19:58:04 +0800

    fixed spell error.

--- NetEase News/Sources/neteasenews.py
+++ NetEase News/Sources/neteasenews.py
@@ -14,48 +14,48 @@ def set_cache(query):
     """
     if query == "headline":
         result = requests.get('http://c.m.163.com/nc/article/headline/T1348647853363/0-20.html').json()
-        respone = result['T1348647853363']
+        response = result['T1348647853363']
         cache1 = []
-        for i in range(len(respone)):
-            if 'url_3w' in respone[i].keys():
-                cache1.append(dict(title=respone[i]['title'], digest=respone[i]['digest'],
-                                   url=respone[i]['url_3w']))
+        for i in range(len(response)):
+            if 'url_3w' in response[i].keys():
+                cache1.append(dict(title=response[i]['title'], digest=response[i]['digest'],
+                                   url=response[i]['url_3w']))
         alfred.cache.set('headline.list', cache1, expire=600)
     elif query == "sport":
         result = requests.get('http://c.m.163.com/nc/article/list/T1348649079062/0-20.html').json()
-        respone = result['T1348649079062']
+        response = result['T1348649079062']
         cache2 = []
-        for i in range(len(respone)):
-            if 'url_3w' in respone[i].keys():
-                cache2.append(dict(title=respone[i]['title'], digest=respone[i]['digest'],
-                                   url=respone[i]['url_3w']))
+        for i in range(len(response)):
+            if 'url_3w' in response[i].keys():
+                cache2.append(dict(title=response[i]['title'], digest=response[i]['digest'],
+                                   url=response[i]['url_3w']))
         alfred.cache.set('sport.list', cache2, expire=600)
     elif query == "finance":
         result = requests.get('http://c.m.163.com/nc/article/list/T1348648756099/0-20.html').json()
-        respone = result['T1348648756099']
+        response = result['T1348648756099']
         cache3 = []
-        for i in range(len(respone)):
-            if 'url_3w' in respone[i].keys():
-                cache3.append(dict(title=respone[i]['title'], digest=respone[i]['digest'],
-                                   url=respone[i]['url_3w']))
+        for i in range(len(response)):
+            if 'url_3w' in response[i].keys():
+                cache3.append(dict(title=response[i]['title'], digest=response[i]['digest'],
+                                   url=response[i]['url_3w']))
         alfred.cache.set('finance.list', cache3, expire=600)
     elif query == "gossip":
         result = requests.get('http://c.m.163.com/nc/article/list/T1348648517839/0-20.html').json()
-        respone = result['T1348648517839']
+        response = result['T1348648517839']
         cache4 = []
-        for i in range(len(respone)):
-            if 'url_3w' in respone[i].keys():
-                cache4.append(dict(title=respone[i]['title'], digest=respone[i]['digest'],
-                                   url=respone[i]['url_3w']))
+        for i in range(len(response)):
+            if 'url_3w' in response[i].keys():
+                cache4.append(dict(title=response[i]['title'], digest=response[i]['digest'],
+                                   url=response[i]['url_3w']))
         alfred.cache.set('gossip.list', cache4, expire=600)
     elif query == "keji":
         result = requests.get('http://c.m.163.com/nc/article/list/T1348649580692/0-20.html').json()
-        respone = result['T1348649580692']
+        response = result['T1348649580692']
         cache5 = []
-        for i in range(len(respone)):
-            if 'url_3w' in respone[i].keys():
-                cache5.append(dict(title=respone[i]['title'], digest=respone[i]['digest'],
-                                   url=respone[i]['url_3w']))
+        for i in range(len(response)):
+            if 'url_3w' in response[i].keys():
+                cache5.append(dict(title=response[i]['title'], digest=response[i]['digest'],
+                                   url=response[i]['url_3w']))
         alfred.cache.set('keji.list', cache5, expire=600)
 
 
